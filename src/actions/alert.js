4503b5d9d2fff063f8f4b48512f9725164da82cc
an-apluss <anuoluwapoakinseye@gmail.com>
2020-06-03 10:15:35 +0100

    make alert message timeout vary

--- src/actions/alert.js
+++ src/actions/alert.js
@@ -2,12 +2,12 @@ import { v4 as uuid_v4 } from "uuid";
 
 import { SET_ALERT, REVOME_ALERT } from "./type";
 
-export const setAlert = (msg, alertType) => (dispatch) => {
+export const setAlert = (msg, alertType, timeout = 5000) => (dispatch) => {
   const id = uuid_v4();
   dispatch({
     type: SET_ALERT,
     payload: { id, msg, alertType },
   });
 
-  setTimeout(() => dispatch({ type: REVOME_ALERT, payload: id }), 5000);
+  setTimeout(() => dispatch({ type: REVOME_ALERT, payload: id }), timeout);
 };
