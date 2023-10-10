bc9c2aa90512fbf79a1b14eb4813ec1efdd329ac
Anuoluwapo Akinseye <anuoluwapo.akinseye@sterling.ng>
2023-10-10 16:25:15 +0100

    rename directory from dashboard to dash-board

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Profile from "./components/profile/Profile";
-import Dashboard from "./components/dashboard/Dashboard";
+import Dashboard from "./components/dash-board/Dashboard";
 import CreateProfile from "./components/profile-forms/CreateProfile";
 import EditProfile from "./components/profile-forms/EditProfile";
 import AddExperience from "./components/profile-forms/AddExperience";
